bc240d2cfbd737c40e43f8f5e7730e82157599ec
abitravers1989 <atravers4007@gmail.com>
2017-07-27 01:06:32 +0100

    Fizzbuzz updated to print number. Challenge complete.

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,10 +1,11 @@
 def fizzbuzz(blah)
   if blah %3 ==0 && blah % 5 ==0
     'fizzbuzz'
-
   elsif blah % 3 == 0
     'fizz'
-  else
+  elsif blah % 5 ==0 
     'buzz'
+  else
+    "#{blah}"
   end
 end
